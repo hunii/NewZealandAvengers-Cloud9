1dd17792ddad97cf0420adcde7c5b92b01a7d78e
Taehyun Kim <pztnakro@gmail.com>
2017-05-24 12:55:43 +1200

    Add main frame page

--- src/nz/ac/aut/ense701/main/Main.java
+++ src/nz/ac/aut/ense701/main/Main.java
@@ -1,6 +1,7 @@
 package nz.ac.aut.ense701.main;
 
 import nz.ac.aut.ense701.gameModel.Game;
+import nz.ac.aut.ense701.gui.GameStartFrame;
 import nz.ac.aut.ense701.gui.KiwiCountUI;
 
 /**
@@ -19,16 +20,19 @@ public class Main
     public static void main(String[] args) 
     {
         // create the game object
-        final Game game = new Game();
+        //final Game game = new Game();
         // create the GUI for the game
-        final KiwiCountUI  gui  = new KiwiCountUI(game);
+        //final KiwiCountUI  gui  = new KiwiCountUI(game);
+        // Create the Game Start Frame
+        final GameStartFrame gStartFrame = new GameStartFrame();
         // make the GUI visible
         java.awt.EventQueue.invokeLater(new Runnable() 
         {
             @Override
             public void run() 
             {
-                gui.setVisible(true);
+                //gui.setVisible(true);
+                gStartFrame.setVisible(true);
             }
         });
     }
